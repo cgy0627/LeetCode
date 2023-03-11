c2a9750e8384a828b2cbd5afde5eb0409916eadc
chaenny <55884436+cgy0627@users.noreply.github.com>
2023-03-11 19:38:32 +0900

    Time: 30 ms (73.06%), Space: 13.8 MB (45.04%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
@@ -1,30 +1,3 @@
 class Solution:
     def isStrictlyPalindromic(self, n: int) -> bool:
-                    
-        def num_base(num, base):
-            ans = ''
-            
-            while num:
-                num, m = divmod(num, base)
-                ans = str(m) + ans
-
-            return ans
-
-        def is_num_palindromic(num):
-            num = str(num)
-            length = len(num)
-
-            if length % 2 == 0:   # even
-                if num[:length//2] == num[length//2:][::-1]:
-                    return True
-            else:
-                if num[:length//2] == num[length//2 + 1:][::-1]:
-                    return True
-
-            return False
-        
-        for i in range(2, n-1):
-            if not is_num_palindromic(num_base(n, i)):
-                return False
-        
-        return True
+        return False
